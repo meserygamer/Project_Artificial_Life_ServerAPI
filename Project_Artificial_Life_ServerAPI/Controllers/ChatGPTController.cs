30c606258dfa5ba4fa882e2c87cd059064d76f6c
MeseryGG <Meserygamer@gmail.com>
2024-03-03 01:09:30 +0300

    Заготовка под контроллер ChatGPT

---
+++ Project_Artificial_Life_ServerAPI/Controllers/ChatGPTController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Artificial_Life_ServerAPI.Controllers
+{
+    [ApiController]
+    [Route("chatGPT/")]
+    public class ChatGPTController : Controller
+    {
+        [HttpPost]
+        [Route("sendMessageToChatBot")]
+        public IActionResult SendMessageToChatBot()
+        {
+            return View();
+        }
+    }
+}
