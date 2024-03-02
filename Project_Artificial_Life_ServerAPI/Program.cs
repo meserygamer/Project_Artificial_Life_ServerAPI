b6b7fcd897d4618145bd05913b03b043237d1ec9
MeseryGG <Meserygamer@gmail.com>
2024-03-03 01:04:34 +0300

    Первичная настройка Swagger

--- Project_Artificial_Life_ServerAPI/Program.cs
+++ Project_Artificial_Life_ServerAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace Project_Artificial_Life_ServerAPI
 {
     public class Program
@@ -8,11 +10,19 @@ namespace Project_Artificial_Life_ServerAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddCors();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Project Artificial Life API",
+                    Description = "промежуточное API проекта PAL, для взаимодействия с нейросетями"
+                });
+            });
 
             var app = builder.Build();
 
@@ -20,12 +30,14 @@ namespace Project_Artificial_Life_ServerAPI
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+            app.UseCors(builder => builder.AllowAnyOrigin());
 
 
             app.MapControllers();
